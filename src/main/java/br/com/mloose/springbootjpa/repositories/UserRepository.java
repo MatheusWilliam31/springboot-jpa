107e4b009971a98b422514ab0da9e0e3c04ac020
Matheus Loose <matheus-loose@m.loose>
2021-07-05 09:36:38 -0300

    Created findAll, findById

--- src/main/java/br/com/mloose/springbootjpa/repositories/UserRepository.java
+++ src/main/java/br/com/mloose/springbootjpa/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.mloose.springbootjpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.mloose.springbootjpa.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
